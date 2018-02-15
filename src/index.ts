a345b786a6853b40920c86b211b1f8272810a29a
j- <j@skeoh.com>
2018-02-15 14:26:59 +1100

    Add function types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ type InterpolationScalar = string | number;
 type InterpolationFunction<P> = (arg: P) => Interpolation<P>;
 export type Interpolation<P = void> = InterpolationScalar | InterpolationFunction<P>;
 
-export const style = <P = {}>(/* Extensions will go here */) => (
-	(strings: string[] | TemplateStringsArray, ...interpolations: Interpolation<P>[]) => {}
+type TemplateFunction<P> = (strings: string[] | TemplateStringsArray, ...interpolations: Interpolation<P>[]) => void;
+type StyleFunction = <P>() => TemplateFunction<P>;
+
+export const style: StyleFunction = <P = {}>(/* Extensions will go here */): TemplateFunction<P> => (
+	(strings, ...interpolations) => {/* Function logic will go here */}
 );
